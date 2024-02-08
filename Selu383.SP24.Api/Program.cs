92f6765b1e4215555ec5f3169600b2976503b33d
Alexander C <eachristian335@gmail.com>
2024-02-07 18:40:46 -0600

    react-web integration

--- Selu383.SP24.Api/Program.cs
+++ Selu383.SP24.Api/Program.cs
@@ -46,6 +46,28 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app 
+    .UseRouting();
+app.UseEndpoints(x =>
+{
+    x.MapControllers();
+
+});
+
+app.UseStaticFiles();
+
+ if (app.Environment.IsDevelopment())
+{
+    app.UseSpa(x =>
+    {
+        x.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+    });
+}
+else
+{
+    app.MapFallbackToFile("index.html");
+}
+
 app.UseAuthorization();
 
 app.MapControllers();
