35512233d97b1bc93d0a80dc67520a195befad21
Jai Bronw <jai.brown@selu.edu>
2024-02-14 16:08:55 -0600

    started authentication controller

---
+++ Selu383.SP24.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Selu383.SP24.Api.Controllers
+{
+    public class AuthenticationController: Controller
+    {
+        private readonly UserManager<User> userManager;
+
+        public AuthenticationController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            userManager.CreateAsync(new User
+            {
+                Email = string.Empty,
+            }, "Password123"
+            );
+            return View();
+        }
+    }
+}
